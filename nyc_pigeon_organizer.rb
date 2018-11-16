e57a4c18a38c655b0ebf6c66141ba0793e231883
Samuel Prada <jspradac@gmail.com>
2018-11-16 15:13:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |category, stats|
+    stats.each do |stat, names|
+      names.each do |name|
+        if !((pigeon_list.keys).include?(name))
+          pigeon_list[name] = {category => ["#{stat}"]}
+        else 
+          if (pigeon_list[name].keys).include?(category)
+            pigeon_list[name][category] << "#{stat}"
+          else 
+            pigeon_list[name][category] = ["#{stat}"]
+          end 
+        end 
+      end 
+    end 
+  end 
+  pigeon_list
 end
